0d54fde8ad4c85e879c5b9d93a2409897f4fa85a
Marcio Lima <mdlima.github@mailnull.com>
2018-05-17 13:01:07 -0300

    Change DHTSENSOR_DATA feature id

--- src/ConfigurableFirmata.h
+++ src/ConfigurableFirmata.h
@@ -73,11 +73,11 @@
 #define SERIAL_MESSAGE          0x60 // communicate with serial devices, including other boards
 #define ENCODER_DATA            0x61 // reply with encoders current positions
 #define ACCELSTEPPER_DATA       0x62 // control a stepper motor
-#define DHTSENSOR_DATA          0x66 // connect to DHT11 or DHT22 sensor
 #define SERVO_CONFIG            0x70 // set max angle, minPulse, maxPulse, freq
 #define STRING_DATA             0x71 // a string message with 14-bits per char
 #define STEPPER_DATA            0x72 // control a stepper motor
 #define ONEWIRE_DATA            0x73 // send an OneWire read/write/reset/select/skip/search request
+#define DHTSENSOR_DATA          0x74 // connect to DHT11 or DHT22 sensor
 #define SHIFT_DATA              0x75 // a bitstream to/from a shift register
 #define I2C_REQUEST             0x76 // send an I2C read/write request
 #define I2C_REPLY               0x77 // a reply to an I2C read request
